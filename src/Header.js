188e9f8ba8a4ce326f9cd6b6ce3e46bfc03f631e
unknown <kulyanasun@gmail.com>
2021-12-25 18:08:44 +0100

    Header Component

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import { IconButton } from '@material-ui/core';
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        <div className='header'>
+            <IconButton>
+            <PersonIcon fontSize='Large' className='header_icon'></PersonIcon>
+            </IconButton>
+            <img src='https://1000logos.net/wp-content/uploads/2018/07/Tinder_logo_PNG3.png' className='header_logo'>
+            
+            </img>
+            <IconButton><ForumIcon fontSize='Large'></ForumIcon></IconButton>
+            
+        </div>
+    )
+}
+
+
+export default Header;
